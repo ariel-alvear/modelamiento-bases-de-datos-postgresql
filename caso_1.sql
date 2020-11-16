223de2795d2e7166af0d87e93776ced0d5e44a70
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-11-16 11:50:36 -0300

    simplificamos el modelo del caso 1, habiamos creado una tabla demás

--- caso_1.sql
+++ caso_1.sql
@@ -19,25 +19,10 @@ CREATE TABLE workers_table(
 CREATE TABLE salaries_table(
     id SERIAL,
     worker_dni VARCHAR(10),
+    drive_file VARCHAR(30),
     PRIMARY KEY (id),
     FOREIGN KEY (worker_dni) REFERENCES workers_table(id)
 );
-
-CREATE TABLE drive_files_table(
-    id SERIAL,
-    file_name VARCHAR(30) UNIQUE,
-    salary_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (salary_id) REFERENCES salaries_table(id)
-);
-
-ALTER TABLE salaries_table
-ADD drive_file VARCHAR(30);
-
-ALTER TABLE salaries_table
-ADD CONSTRAINT drive_file
-FOREIGN KEY (drive_file) 
-REFERENCES drive_files_table(file_name);
 
 
 --ahora agregaremos datos falsos para probar que este todo OK.
@@ -52,28 +37,14 @@ INSERT INTO workers_table (id, worker_name, worker_lastname, direction, departam
 INSERT INTO workers_table (id, worker_name, worker_lastname, direction, departament_id) VALUES ('17700000-3', 'tomas', 'hernandez', 'salesianos 300, san miguel', 3);
 
 --3 registros en tabla salarios
-INSERT INTO salaries_table (worker_dni) VALUES ('17700000-1');
-INSERT INTO salaries_table (worker_dni) VALUES ('17700000-2');
-INSERT INTO salaries_table (worker_dni) VALUES ('17700000-3');
+INSERT INTO salaries_table (worker_dni, drive_file) VALUES ('17700000-1', 'sueldo_marzo_17700000-1');
+INSERT INTO salaries_table (worker_dni, drive_file) VALUES ('17700000-1', 'sueldo_abril_17700000-1');
+INSERT INTO salaries_table (worker_dni, drive_file) VALUES ('17700000-1', 'sueldo_mayo_17700000-1');
+INSERT INTO salaries_table (worker_dni, drive_file) VALUES ('17700000-2', 'sueldo_marzo_17700000-2');
+INSERT INTO salaries_table (worker_dni, drive_file) VALUES ('17700000-3', 'sueldo_marzo_17700000-3');
 
---3 registros en tabla archivos
-INSERT INTO drive_files_table(file_name) VALUES ('sueldo_marzo_17700000-1');
-INSERT INTO drive_files_table(file_name) VALUES ('sueldo_marzo_17700000-2');
-INSERT INTO drive_files_table(file_name) VALUES ('sueldo_marzo_17700000-3');
-
---3 registros para terminar actualizar la tabla salarios
-UPDATE salaries_table drive_file SET drive_file='sueldo_marzo_17700000-1' WHERE id=1;
-UPDATE salaries_table drive_file SET drive_file='sueldo_marzo_17700000-2' WHERE id=2;
-UPDATE salaries_table drive_file SET drive_file='sueldo_marzo_17700000-3' WHERE id=3;
-
-
---3 registros para actualizar la tabla archivos
-UPDATE drive_files_table salary_id SET salary_id=1 WHERE id=1;
-UPDATE drive_files_table salary_id SET salary_id=2 WHERE id=2;
-UPDATE drive_files_table salary_id SET salary_id=3 WHERE id=3;
 
 --revisamos
 SELECT * FROM departaments_table;
 SELECT * FROM workers_table;
 SELECT * FROM salaries_table;
-SELECT * FROM drive_files_table;
