81c5ce5efa1aa18b7441a5fb0aa4a4ee4cc13e5c
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-11-16 10:49:25 -0300

    primer caso listo

--- caso_1.sql
+++ caso_1.sql
@@ -18,17 +18,23 @@ CREATE TABLE workers_table(
 
 CREATE TABLE salaries_table(
     id INT,
-    drive_file VARCHAR(20),
     worker_dni INT,
     PRIMARY KEY (id),
-    FOREIGN KEY (worker_dni) REFERENCES workers_table(id),
-    FOREIGN KEY (drive_file) REFERENCES drive_files_table(file_name)
+    FOREIGN KEY (worker_dni) REFERENCES workers_table(id)
 );
 
 CREATE TABLE drive_files_table(
     id INT,
-    file_name VARCHAR(20),
+    file_name VARCHAR(20) UNIQUE,
     salary_id INT,
     PRIMARY KEY (id),
     FOREIGN KEY (salary_id) REFERENCES salaries_table(id)
 );
+
+ALTER TABLE salaries_table
+ADD drive_file VARCHAR(20);
+
+ALTER TABLE salaries_table
+ADD CONSTRAINT drive_file
+FOREIGN KEY (drive_file) 
+REFERENCES drive_files_table(file_name);
